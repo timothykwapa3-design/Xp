4dd5def7d89ec789aee7234f91b1e06de028ddf0
timothykwapa3-design <timothykwapa3@gmail.com>
2026-05-24 12:16:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -183,3 +183,11 @@
 
     renderCart();
 </script>
+
+<script>
+    function updateCartCount() {
+        const cart = JSON.parse(localStorage.getItem('ogCart') || '[]');
+        document.getElementById('cartCount').textContent = cart.reduce((a, i) => a + i.qty, 0);
+    }
+    updateCartCount();
+</script>
